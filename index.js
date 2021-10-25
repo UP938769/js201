6bb4c528236f79cd8f399196fb6c5fd11f3187dd
Ryan Harris <UP938769@uni.ds.port.ac.uk>
2021-10-25 17:49:27 +0100

    Completed first 6 challenges

--- index.js
+++ index.js
@@ -13,15 +13,15 @@
 'use strict';
 
 function id() {
-  // e.g. return "UP654321";
+  return "UP938769";
 }
 
 function fn() {
-  return 'Replace This With Your First Name';
+  return 'Ryan';
 }
 
 function sn() {
-  return 'Replace This With Your Surname';
+  return 'Edwards';
 }
 
 function example() {
@@ -29,4 +29,25 @@ function example() {
   // your first function then
   // add more below as necessary.
 
+}
+
+// Create an add function that accepts two parameters called a and b, and returns their sum.
+
+function add (a, b){
+  const sum = a + b;
+  return sum;
+}
+
+// Create a subtract function that accepts two parameters, a and b, and subtracts b from a and returns the result. 
+
+function subtract (a, b){
+  const sum = a - b;
+  return sum;
+}
+
+// Create a checkObject function that accepts an object called obj as a parameter and sets the object's property `checked` to `true`.
+
+
+function checkObject (obj){
+  // I need to set obj property named checked to true
 }
